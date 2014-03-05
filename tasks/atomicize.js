b4452117ba8a0bc31e1f98b4471b860d6367f508
Josh Kaplan <josh.kaplan@bestbuy.com>
2014-03-05 15:49:03 -0600

    Fixed element parsing to be correctly recursive through the DOM

--- tasks/atomicize.js
+++ tasks/atomicize.js
@@ -5,49 +5,62 @@
  * Copyright (c) 2014 Josh Kaplan
  * Licensed under the MIT license.
  */
-
 'use strict';
 
 var htmlparser = require('htmlparser'),
     config = require('../lib/config');
 
-module.exports = function(grunt) {
-  grunt.registerMultiTask('atomicize', 'The best Grunt plugin ever.', function() {
+module.exports = function (grunt) {
+  grunt.registerMultiTask('atomicize', 'The best Grunt plugin ever.', function () {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({});
     var outputMap;
+
+    function handleClass(className) {
+      var splitClass = className.split('-');
+      if (splitClass.length > 1) {
+        if (config[splitClass[0]]) {
+          var configBlock = config[splitClass[0]];
+          Object.keys(configBlock.params).forEach(function (regexString) {
+            var regexp = new RegExp(regexString, "gi");
+            if (regexp.test(splitClass[1])) {
+              if (!outputMap[className]) {
+                var cssString = splitClass[1].replace(regexp, configBlock.params[regexString]);
+                outputMap[className] = configBlock.expand + ": " + cssString + ";";
+              }
+            }
+          });
+        }
+      }
+    }
+
     var htmlHandler = new htmlparser.DefaultHandler(function (error, dom) {
       if (error) {
         grunt.log.warn('There was an error', error);
         return;
       }
 
-      var classes = dom[0].attribs.class.split(" ");
-      classes.forEach(function (className) {
-        var splitClass = className.split('-');
-        if (splitClass.length > 1) {
-          if (config[splitClass[0]]) {
-            var configBlock = config[splitClass[0]];
-            Object.keys(configBlock.params).forEach(function (regexString) {
-              var regexp = new RegExp(regexString, "gi");
-              if (regexp.test(splitClass[1])) {
-                if (!outputMap[className]) {
-                  var cssString = splitClass[1].replace(regexp, configBlock.params[regexString]);
-                  outputMap[className] = configBlock.expand + ": " + cssString + ";";
-                }
-              }
-            });
-          }
+      function handleElement(domElement) {
+        if (domElement.type === "tag" && domElement.attribs && domElement.attribs.class) {
+          var classes = domElement.attribs.class.split(" ");
+          classes.forEach(handleClass);
         }
-      });
+
+        if (domElement.children) {
+          domElement.children.forEach(handleElement);
+        }
+      }
+
+      dom.forEach(handleElement);
+
     });
     var parser = new htmlparser.Parser(htmlHandler);
 
     // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
+    this.files.forEach(function (f) {
       outputMap = {};
       // Concat specified files.
-      f.src.filter(function(filepath) {
+      f.src.filter(function (filepath) {
         // Warn on and remove invalid source files (if nonull was set).
         if (!grunt.file.exists(filepath)) {
           grunt.log.warn('Source file "' + filepath + '" not found.');
@@ -55,7 +68,7 @@ module.exports = function(grunt) {
         } else {
           return true;
         }
-      }).forEach(function(filepath) {
+      }).forEach(function (filepath) {
         // Read file source.
         var fileContents = grunt.file.read(filepath);
         parser.parseComplete(fileContents);
