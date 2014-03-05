96f4fce1f99a5bd33a05e930a53628cea5d526bf
Josh Kaplan <josh.kaplan@bestbuy.com>
2014-03-05 14:21:52 -0600

    Added basic HTML parsing and CSS generation
    Stil need to write the parser for the emmett config. This is currently
    stubbed out in configParser.

--- tasks/atomicize.js
+++ tasks/atomicize.js
@@ -8,22 +8,47 @@
 
 'use strict';
 
+var htmlparser = require('htmlparser'),
+    config = require('../lib/configParser').getConfig();
+
 module.exports = function(grunt) {
-
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
   grunt.registerMultiTask('atomicize', 'The best Grunt plugin ever.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+    var options = this.options({});
+    var outputMap;
+    var htmlHandler = new htmlparser.DefaultHandler(function (error, dom) {
+      if (error) {
+        grunt.log.warn('There was an error', error);
+        return;
+      }
+
+      var classes = dom[0].attribs.class.split(" ");
+
+      classes.forEach(function (className) {
+        var splitClass = className.split('-');
+        if (splitClass.length > 1) {
+          if (config[splitClass[0]]) {
+            var configBlock = config[splitClass[0]];
+            Object.keys(configBlock.params).forEach(function (regexString) {
+              var regexp = new RegExp(regexString, "gi");
+              if (regexp.test(splitClass[1])) {
+                if (!outputMap[className]) {
+                  var cssString = splitClass[1].replace(regexp, configBlock.params[regexString]);
+                  outputMap[className] = configBlock.expand + ": " + cssString + ";";
+                }
+              }
+            });
+          }
+        }
+      });
     });
+    var parser = new htmlparser.Parser(htmlHandler);
 
     // Iterate over all specified file groups.
     this.files.forEach(function(f) {
+      outputMap = {};
       // Concat specified files.
-      var src = f.src.filter(function(filepath) {
+      f.src.filter(function(filepath) {
         // Warn on and remove invalid source files (if nonull was set).
         if (!grunt.file.exists(filepath)) {
           grunt.log.warn('Source file "' + filepath + '" not found.');
@@ -31,20 +56,28 @@ module.exports = function(grunt) {
         } else {
           return true;
         }
-      }).map(function(filepath) {
+      }).forEach(function(filepath) {
         // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+        var fileContents = grunt.file.read(filepath);
+        parser.parseComplete(fileContents);
+      });
 
-      // Handle options.
-      src += options.punctuation;
+      var output = [];
+
+      Object.keys(outputMap).forEach(function (className) {
+        output.push("\t." + className + "{");
+        output.push("\t\t" + outputMap[className]);
+        output.push("\t}");
+      });
+
+      output.unshift("body {");
+      output.push("}");
 
       // Write the destination file.
-      grunt.file.write(f.dest, src);
+      grunt.file.write(f.dest, output.join("\n"));
 
       // Print a success message.
       grunt.log.writeln('File "' + f.dest + '" created.');
     });
   });
-
 };
